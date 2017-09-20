ebbba3543b397372d672afde15d0a037dd8377d1
Markin Andrey <magazon4ik@yandex.ru>
2017-09-20 13:19:55 +0300

    changed

---
+++ app/src/main/java/app/education/flagquiz/MainActivityFragment.java
@@ -0,0 +1,9 @@
+package app.education.flagquiz;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author Markin Andrey on 20.09.17.
+ */
+public class MainActivityFragment extends Fragment {
+}
