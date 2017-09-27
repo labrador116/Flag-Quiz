778976b2b7752158aff19f3e5772d9c383b08e3e
Andrey M <magazon4ik@yandex.ru>
2017-09-27 10:05:15 +0300

    changed

---
+++ app/src/main/java/app/education/flagquiz/SettingActivity.java
@@ -0,0 +1,9 @@
+package app.education.flagquiz;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Markin Andrey on 27.09.2017.
+ */
+public class SettingActivity extends MainActivity {
+}
---
+++ app/src/main/java/app/education/flagquiz/SettingFragment.java
@@ -0,0 +1,9 @@
+package app.education.flagquiz;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * @author Markin Andrey on 27.09.2017.
+ */
+public class SettingFragment extends PreferenceFragment {
+}
