4f16220bafcb3901539c3953a9f20bf5895d8c22
Andrey M <magazon4ik@yandex.ru>
2017-09-28 10:02:00 +0300

    changed

--- app/src/main/java/app/education/flagquiz/SettingActivity.java
+++ app/src/main/java/app/education/flagquiz/SettingActivity.java
@@ -1,9 +1,19 @@
 package app.education.flagquiz;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * @author Markin Andrey on 27.09.2017.
  */
-public class SettingActivity extends MainActivity {
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 }
--- app/src/main/java/app/education/flagquiz/SettingFragment.java
+++ app/src/main/java/app/education/flagquiz/SettingFragment.java
@@ -1,9 +1,15 @@
 package app.education.flagquiz;
 
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 /**
  * @author Markin Andrey on 27.09.2017.
  */
 public class SettingFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences); // load from XML
+    }
 }
